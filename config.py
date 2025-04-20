b6785cd5d626a0d2304a8f276fbe5a183272e953
Konstantin_Fedorahin <konstantin.fedorahin@yandex.ru>
2025-04-20 18:55:16 +0500

    Немножко доработал

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-CHANNEL_ID = ""
-CHAT_ID = ""
+CHANNEL_ID = "@testovy_chan"
+CHAT_ID = "@testovy_chat"
 
 ADMIN_ID = ""
 
-with open("filter_profanity_russian.txt") as file:
-    WORDS = [row.strip() for row in file]
+# with open("filter_profanity_russian.txt") as file:
+#     WORDS = [row.strip() for row in file]
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from aiogram import dispatcher, types, Bot
+from aiogram import Dispatcher, types, Bot
 import config as conf
 import telebot
 
 bot = telebot.TeleBot('7893229260:AAG34zlGRxu7x9DgSA91zXD7WGL8Y3KdCbo')
-dp = dispatcher(bot)
+dp = Dispatcher(bot)
 
 @dp.message_handler()
 async def mess_handler(message: types.Message):
@@ -11,6 +11,10 @@ async def mess_handler(message: types.Message):
     for word in conf.WORDS:
         if word in text:
             await message.delete()
+
+@dp.message_handler(commands=["test"], commands_prefix="/")
+async def test(message: types.Message):
+    await bot.send_mesage(message.from_user.id, f"ID: {message.from_user.id}")
 
 @bot.message_handler(commands=['start'])
 def main(message):
